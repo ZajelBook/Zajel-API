ae9b38331e327c8e7195850dc0e60c88025a3700
Mohamad <mrahmoun93@gmail.com>
2021-06-11 03:07:39 +0800

    restart sidekiq after deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
+      execute 'systemctl --user restart sidekiq'
       execute :touch, release_path.join('tmp/restart.txt')
     end
   end
