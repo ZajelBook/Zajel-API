d08167eead951be3ce3dbc971e8bff3a47fb065c
Mohamad <mrahmoun93@gmail.com>
2021-06-11 02:48:45 +0800

    protect sidekiq UI with username and password

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 require 'sidekiq/web'
 
+Sidekiq::Web.use ActionDispatch::Cookies
+Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
+
 Rails.application.routes.draw do
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_AUTH_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_AUTH_PASSWORD"]))
+  end if Rails.env.production?
+
   mount Sidekiq::Web => '/sidekiq'
 
   devise_for :users
