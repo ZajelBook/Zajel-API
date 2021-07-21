2af2c879b72cc17f8e1e67b822a72362f1294bf8
Mohamad <mrahmoun93@gmail.com>
2021-07-22 00:27:42 +0800

    remove rpush service

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,6 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       execute 'systemctl --user restart zajel-sidekiq'
-      execute 'systemctl --user restart zajel-rpush'
       execute :touch, release_path.join('tmp/restart.txt')
     end
   end
