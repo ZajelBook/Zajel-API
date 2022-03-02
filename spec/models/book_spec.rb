cc29f76d1aca4045231ec79cb8ffd7248e752621
Mohamad <mrahmoun93@gmail.com>
2022-03-02 02:33:59 +0100

    fix failing book_spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Book do
+  before { allow_any_instance_of(User).to receive(:send_confirmation_code).and_return(true) }
+
   describe 'create a new book' do
     context 'when owner is verified' do
-      let(:book) { build :book, owner: build(:verified_user) }
+      let(:book) { create :book, owner: build(:verified_user) }
 
       it { expect(book.approved).to be_truthy }
     end
 
     context 'when owner is not verified' do
-      let(:book) { build :book }
+      let(:book) { create :book }
 
       it { expect(book.approved).to be_falsey }
     end
