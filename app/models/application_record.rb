99d6312cbe7d00a0c7b204511cd84593a5058ad5
Mohamad <mrahmoun93@gmail.com>
2022-05-17 23:18:54 +0200

    Use background jobs to send book activity notifications

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,8 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def notify_admins(message)
+    return if Rails.env.test?
+
     SendTelegramNotificationJob.perform_later(message)
   end
 end
