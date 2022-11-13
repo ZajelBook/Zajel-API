7f73fa86ea85e74347e127d7a7a24aac93828e6f
Mohamad <mrahmoun93@gmail.com>
2022-11-13 03:46:15 +0100

    use session store and remove devise_rails_api patch

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module ZajelApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.session_store :cookie_store, key: '_zajel_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
     config.api_only = true
 
     config.autoload_paths   +=  %W(#{config.root}/lib)
