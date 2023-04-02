26d23b74b387b408ee8b7ccc369dd0a2f53c88b0
Mohamad <mrahmoun93@gmail.com>
2023-04-03 00:27:30 +0300

    fix failing specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session, unless: -> { request.format.json? }
+
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    current_user.update_columns(locale: request.headers['locale']) if current_user && current_user.locale != request.headers['locale']
+    locale = request.headers['locale'] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 end
