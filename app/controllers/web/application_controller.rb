4daae7fb3ed8a39b3f2672a8f519387833ebc726
Mohamad <mrahmoun93@gmail.com>
2023-04-11 03:34:40 +0300

    do not set location when accessing static pages

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -4,7 +4,7 @@ class Web::ApplicationController < ActionController::Base
 
   before_action :set_cookies
   before_action :turbo_frame_request_variant
-  before_action :set_location
+  before_action :set_location, unless: :static_pages_controller?
 
   protected
   def turbo_frame_request_variant
@@ -20,5 +20,11 @@ class Web::ApplicationController < ActionController::Base
     return unless current_user
 
     current_user.update(latitude: cookies['latitude'], longitude: cookies['longitude'])
+  end
+
+  private
+
+  def static_pages_controller?
+    is_a?(Web::StaticPagesController)
   end
 end
